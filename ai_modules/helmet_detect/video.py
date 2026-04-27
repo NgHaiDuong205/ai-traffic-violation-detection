9f5de10d28d4a221a844af1d28519a03f115bcbc
Nguyen Hai Duong <duongnh0611@gmail.com>
2026-04-27 22:08:16 +0700

    web

--- ai_modules/helmet_detect/video.py
+++ ai_modules/helmet_detect/video.py
@@ -3,10 +3,7 @@ import cv2
 from tkinter import filedialog
 import tkinter as tk
 
-# Load model
-model = YOLO("D:/Thuc_tap_co_so/best3.pt")
-
-# Chọn video
+model = YOLO("D:/ai-traffic-violation-detection/ai_modules/helmet_detect/weights/best3.pt")
 root = tk.Tk()
 root.withdraw()
 
@@ -19,32 +16,17 @@ if video_path:
     cap = cv2.VideoCapture(video_path)
 
     if not cap.isOpened():
-        print("Không mở được video!")
         exit()
-
     while True:
         ret, frame = cap.read()
         if not ret:
             break
-
-        # Resize cho nhanh hơn
         frame = cv2.resize(frame, (640, 480))
-
-        # Predict (cách nhanh)
         results = model(frame, conf=0.25, imgsz=320)
-
-        # Vẽ box
         annotated_frame = results[0].plot()
-
-        # Hiển thị
         cv2.imshow("Helmet Detection - Video", annotated_frame)
-
-        # Nhấn 'q' để thoát
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
     cv2.destroyAllWindows()
-
-else:
-    print("Chưa chọn video!")
